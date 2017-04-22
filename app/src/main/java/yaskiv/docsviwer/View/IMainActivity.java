00b23d675f14126facae2b607b3c2f6466db47a4
DyedfoxRA <dyedfox.ra@gmail.com>
2017-04-22 09:13:04 +0200

    interface buttonclick

--- app/src/main/java/yaskiv/docsviwer/View/IMainActivity.java
+++ app/src/main/java/yaskiv/docsviwer/View/IMainActivity.java
@@ -1,8 +1,13 @@
 package yaskiv.docsviwer.View;
 
+import android.view.View;
+
 /**
  * Created by yaski on 22.04.2017.
  */
 
 public interface IMainActivity {
+     void ButtonClick (View view);
+
+
 }
--- app/src/main/java/yaskiv/docsviwer/View/Impl/MainActivity.java
+++ app/src/main/java/yaskiv/docsviwer/View/Impl/MainActivity.java
@@ -2,6 +2,7 @@ package yaskiv.docsviwer.View.Impl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import yaskiv.docsviwer.Presenter.Impl.PresenterMain;
 import yaskiv.docsviwer.R;
@@ -15,5 +16,10 @@ public class MainActivity extends AppCompatActivity implements IMainActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pm=new PresenterMain(this);
+    }
+
+    @Override
+    public void ButtonClick(View view) {
+
     }
 }
